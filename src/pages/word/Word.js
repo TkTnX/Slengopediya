075c0f854d4a0e3ae0976c6603687022abf6b955
TkTnX <timur20-09@mail.ru>
2024-01-26 16:03:22 +0500

    пофиксил ошибки

--- src/pages/word/Word.js
+++ src/pages/word/Word.js
@@ -6,6 +6,20 @@ const Word = () => {
   const { index } = useParams();
   const word = words.find((word) => word.id === index);
 
+  if (!word || !word.title) {
+    return (
+      <main>
+        <section className="slovo">
+          <div className="slovo__container">
+            <h2 className="slovo__title">Пожалуйста, вернитесь на главную</h2>
+            <NavLink to="/" className="slovo__btn">
+              <p>Вернуться на главную</p>
+            </NavLink>
+          </div>
+        </section>
+      </main>
+    );
+  }
   return (
     <main>
       <section className="slovo">
